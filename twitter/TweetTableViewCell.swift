f88811466c6b9550da0e598161a7f876c7c564c9
Marco Sgrignuoli <marco.sgrignuoli@gmail.com>
2015-10-04 13:38:11 -0700

    Add pull to refresh and navigation controller

--- twitter/TweetTableViewCell.swift
+++ twitter/TweetTableViewCell.swift
@@ -10,6 +10,30 @@ import UIKit
 
 class TweetTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var userPicture: UIImageView!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var timestamp: UILabel!
+    @IBOutlet weak var message: UILabel!
+    
+    var tweet: Tweet! {
+        didSet {
+            userName.text = tweet.user?.screenname
+            let dateFormatter = NSDateFormatter()
+            dateFormatter.dateFormat = "hh:mm"
+            timestamp.text = dateFormatter.stringFromDate(tweet.createdAt!)
+            message.text = tweet.text
+            if tweet.user?.profileImageUrl != nil {
+                userPicture.setImageWithURL(NSURL(string: (tweet.user?.profileImageUrl)!))
+            }
+        }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+//        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
